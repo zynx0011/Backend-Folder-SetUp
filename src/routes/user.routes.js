4438fecbeac66d0459772bab6f7a479d1c35652a
zynx0011 <kunalnachan10@gmail.com>
2024-01-27 14:08:14 +0530

    updated routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,63 +1,50 @@
 import { Router } from "express";
-import {
-  changeCurrentUserPassword,
-  getCurrentUser,
-  getUserChannelProfile,
-  getWatchHistory,
-  loginUser,
-  logoutuser,
-  refreshAccessToken,
-  registerUser,
-  updateAccoutDetails,
-  updateUserAvatar,
-  updateUserCoverImage,
-  updateUserName,
+import { 
+    loginUser, 
+    logoutUser, 
+    registerUser, 
+    refreshAccessToken, 
+    changeCurrentPassword, 
+    getCurrentUser, 
+    updateUserAvatar, 
+    updateUserCoverImage, 
+    getUserChannelProfile, 
+    getWatchHistory, 
+    updateAccountDetails
 } from "../controllers/user.controller.js";
-import { upload } from "../middlewares/multer.middleware.js";
-import { verifyjwt } from "../middlewares/auth.middleware.js";
+import {upload} from "../middlewares/multer.middleware.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
-const router = Router();
+
+const router = Router()
 
 router.route("/register").post(
-  upload.fields([
-    {
-      name: "avatar",
-      maxCount: 1,
-    },
-    {
-      name: "coverImage",
-      maxCount: 1,
-    },
-  ]),
-  registerUser
-);
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1
+        }, 
+        {
+            name: "coverImage",
+            maxCount: 1
+        }
+    ]),
+    registerUser
+    )
 
-router.route("/login").post(loginUser);
+router.route("/login").post(loginUser)
 
-router.route("/logout").post(verifyjwt, logoutuser);
+//secured routes
+router.route("/logout").post(verifyJWT,  logoutUser)
+router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
 
-router.route("/refresh-token").post(refreshAccessToken);
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
 
-router.route("/change-password").post(verifyjwt, changeCurrentUserPassword);
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/history").get(verifyJWT, getWatchHistory)
 
-router.route("/current-user").get(verifyjwt, getCurrentUser);
-
-router.route("/update-username").patch(verifyjwt, updateUserName);
-
-router.route("/update-account").post(verifyjwt, updateAccoutDetails); // patch is used for single update
-
-router
-  .route("/avatar")
-  .patch(verifyjwt, upload.single("avatar"), updateUserAvatar);
-
-router
-  .route("/cover-image")
-  .patch(verifyjwt, upload.single("coverImage"), updateUserCoverImage);
-
-router.route("/c/:username").get(verifyjwt, getUserChannelProfile);
-
-router.route("/history").get(verifyjwt, getWatchHistory);
-
-// routes for video
-
-export default router;
+export default router
--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -1,23 +1,40 @@
 import { Router } from "express";
+import {
+  deleteVideo,
+  getAllVideos,
+  getVideoById,
+  publishAVideo,
+  togglePublishStatus,
+  updateVideo,
+} from "../controllers/video.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { upload } from "../middlewares/multer.middleware.js";
-import { verifyjwt } from "../middlewares/auth.middleware.js";
-import { uploadVideo } from "../controllers/user.video.controller.js";
+import video from "../middlewares/video.middleware.js";
 
 const router = Router();
+router.use(video);
+router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
-router.route("/upload-video").post(
-  verifyjwt,
-  upload.fields([
-    {
-      name: "video",
-      maxCount: 1,
-    },
-    {
-      name: "thumbnail",
-      maxCount: 1,
-    },
-  ]),
-  uploadVideo
-);
+router
+  .route("/")
+  .get(getAllVideos)
+  .post(
+    upload.fields([
+      {
+        name: "video",
+      },
+      {
+        name: "thumbnail",
+        maxCount: 1,
+      },
+    ]),
+    publishAVideo
+  );
+
+router.route("/c/:videoId").get(getVideoById).delete(deleteVideo);
+
+router.route("/c/:title").patch(upload.single("thumbnail"), updateVideo);
+
+router.route("/toggle/publish/:videoId").patch(togglePublishStatus);
 
 export default router;
